3379fc663db8c339879316db15d863ce27c631f6
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-07-04 20:35:55 +0530

    Add files via upload

---
+++ bin_oct_hex.py
@@ -0,0 +1,35 @@
+'''binary'''
+# print(bin(7))  #0b111
+# print(0b111)  #7
+# print(0B111)  #7
+
+# print(bin(9))  #0b1001
+# print(0b1001)  #9
+
+# print(bin(16))  #0b1000
+
+'''octal'''
+# print(oct(24))  #0o30
+# print(0o30)  #24
+# print(0O30)  #24
+
+# print(oct(23))  #0o27
+# print(0o27)  #23
+# print(0O27)  #23
+
+'''hexa decimal'''
+# print(hex(33))  #0x21
+# print(0x21)  #33
+# print(0X21)  #33
+
+# print(hex(102))  #0x66
+# print(0x66)  #102
+# print(0X66)  #102
+
+# print(0xA)  #10
+# print(0xb)  #11
+# print(0xC)  #12
+# print(0xd)  #13
+# print(0xE)  #14
+# print(0xf)  #15
+# print(0xF)  #15
